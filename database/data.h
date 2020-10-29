6818532dd741fb32d7b1b99c3a771423c157cdb8
chris <chris@quantumthought.io>
2020-10-29 13:14:04 -0400

    Addition of header file

---
+++ database/data.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include "../PQC_IBE/Reference_Implementation_KEM/SABER_params.h"
+
+#define SIZE 200000
+#define BIOBYTES 128
+
+typedef struct identity {
+   u_int8_t ID[SABER_SEEDBYTES];
+} id_t;
+
+typedef struct biometric {
+   u_int8_t BIO[BIOBYTES];
+} bio_t;
+
+typedef struct publicKey {
+   u_int8_t PUB[SABER_PUBLICKEYBYTES];
+} pub_t;
+
+typedef struct Data {
+   id_t *id;
+   bio_t *bio;
+   pub_t *pub;
+} data_t;
+
+typedef struct Database {
+   dataItem database[SIZE];
+} database_t;
+
+data_t init(id_t id, bio_t bio, pub_t pub);
+
+int hash(id_t id);
+
+int exists();
+
+bool validate();
+
+pub_t getPublicKey();
+
+void insert(data_t data, id_t id, bio_t bio, pub_t pub);
+
+void remove(data_t data, id_t id, bio_t bio);
+
+void error(char *msg);
+
+void newProcess(int sock);
+
